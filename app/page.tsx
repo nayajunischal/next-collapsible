fe94f034ff94f1a18a9f9fbd3d07ab499363dd8f
Nischal Shrestha <nischalnayaju.shrestha@gmail.com>
2024-04-03 15:03:28 +0545

    added component title

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-br from-blue-400 to-purple-800 flex-col">
       {/* <CollapsibleComponent /> */}
 
-      <p className="text-white p-3 text-2xl font-thin"> Inspired by <Image src="/radix.svg" alt="Radix Logo" width={100} height={50} className="p-1.5"></Image></p>
+      <p className="text-white p-3 text-2xl font-thin"> Collapsible Component</p>
       <CollapsibleContext />
     </div>
   );
